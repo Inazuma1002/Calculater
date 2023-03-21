4ea26da80e932b55c3319e70820a1286b5c84c59
Manas Gupta <manas.gupta@iiitb.ac.in>
2023-03-21 19:59:28 +0530

    added more functions and test cases.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,22 +1,51 @@
-import java.lang.Math;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Calculator {
 
 
-        float ans;
+    float ans;
         float x, y;
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
 
-        public float add(float x, float y){
+
+    public float add(float x, float y){
             float temp = x + y;
+            logger.info("[ADDITION] - " + x + ", " + y);
+            logger.info("[RESULT - ADDITION] - " + temp);
             return temp;
+
         }
 
         public float increment(float x){
             float temp = x + 1;
             return temp;
         }
-
+    public int factorial(int x){
+        if(x < 0){
+            logger.error("[ERROR - FACTORIAL] - factorial function does not work with -ve number");
+            return -1;
+        }
+        int ans = 1;
+        for(int i = 1; i <= x; i++){
+            ans = ans * i;
+        }
+        logger.info("[FACTORIAL] - " + x);
+        logger.info("[RESULT - FACTORIAL] - " + ans);
+        return ans;
+    }
+    public float square_root(float x){
+        if(x < 0){
+            logger.error("[ERROR - SQRT] - square root function is given -ve number");
+            return -1f;
+        }
+        double temp = Math.sqrt(x);
+        logger.info("[SQRT] - " + x);
+        logger.info("[RESULT - SQRT] - " + temp);
+        return (float) temp;
+    }
 
         public float evaluate(int op, float x, float y){
             switch(op)
@@ -27,6 +56,13 @@ public class Calculator {
                 case(2):
                     ans = add(x,y);
                     break;
+                case(3):
+                    ans= factorial((int)x);
+                    break;
+                case(4):
+                    ans= square_root((int)x);
+                    break;
+
                 default:
                     System.out.println("wrong option selected");
                     break;
@@ -41,6 +77,9 @@ public class Calculator {
             System.out.print("Choose operation \n"+
                     "1. Increment \n"+
                     "2. Addition  \n"+
+                    "3. Factorial \n"+
+                    "4. Square_Root \n"+
+
                     "Enter your choice : ");
 
             int op = scan.nextInt();
@@ -48,6 +87,14 @@ public class Calculator {
             float y = 0;
 
             if (op == 1){
+                System.out.print("Enter operand x: ");
+                x = scan.nextFloat();
+            }
+            if(op == 3){
+                System.out.print("Enter operand x: ");
+                x = scan.nextFloat();
+            }
+            if(op == 4){
                 System.out.print("Enter operand x: ");
                 x = scan.nextFloat();
             }
---
+++ src/test/java/Squareroot_Test.java
@@ -0,0 +1,28 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Squareroot_Test {
+    @BeforeClass
+    public static void before_test() {
+
+        System.out.println("starting Squareroot test");
+
+    }
+
+
+    @Test
+    public void positive_float() {
+        Calculator mycalc = new Calculator();
+        System.out.println("test 1) Squareroot +ve");
+        assertEquals("Squareroot 5 not working correctly",2,mycalc.square_root(4),0.00001);
+    }
+
+    @Test
+    public void negative_float() {
+        Calculator mycalc = new Calculator();
+        System.out.println("test 2) Squareroot -ve");
+        assertEquals("Squareroot -5 not working correctly",-1,mycalc.square_root(-6),0.00001);
+    }
+}
